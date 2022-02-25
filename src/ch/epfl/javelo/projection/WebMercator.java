d981b81f5b5c223f7b838d3263c6c3eba2f5f65b
GustaveCharles <gustave.charles--saigne@epfl.ch>
2022-02-25 14:24:46 +0100

    webmercator finie

--- src/ch/epfl/javelo/projection/WebMercator.java
+++ src/ch/epfl/javelo/projection/WebMercator.java
@@ -1,4 +1,52 @@
+
 package ch.epfl.javelo.projection;
 
-public class WebMercator {
+import ch.epfl.javelo.Math2;
+
+public final class WebMercator {
+
+    /**
+     * private constructor, non instantiable
+     */
+    private WebMercator(){}
+
+    /**
+     * passage de WGS84 à Web Mercator
+     * @param lon
+     * @return
+     */
+    public static double x(double lon){
+
+
+        double X = (lon + Math.PI)/(2*Math.PI);
+
+        return X;
+    }
+
+    public static double y(double lat){
+
+        double Y = (Math.PI - Math2.asinh(Math.tan(lat)));
+
+        return Y;
+    }
+
+    /**
+     * passage de Web Mercator a WGS84
+     * @param x
+     * @return
+     */
+    public static double lon(double x){
+
+        double lon = 2*Math.PI*x - Math.PI;
+
+        return lon;
+    }
+
+    public static double lat(double y){
+
+        double lat = Math.atan(Math.sinh(Math.PI - 2*Math.PI*y));
+
+        return lat;
+    }
+
 }
