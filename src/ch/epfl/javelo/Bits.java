d22fa96f8e4c119b294dc10de14a18bd30b7108f
“bcoispeau” <baudoin.coispeau@epfl.ch>
2022-03-01 14:08:46 +0100

    Functions terminée mais pas testée

--- src/ch/epfl/javelo/Bits.java
+++ src/ch/epfl/javelo/Bits.java
@@ -1,25 +1,31 @@
 package ch.epfl.javelo;
 
 public final class Bits {
-    private Bits () {}
-
-    public static int extractSigned(int value, int start, int length){
-        if (start + length >= 31){
-            throw new IllegalArgumentException();
-        }
-        int shift1 = value << (32-(start+length));
-        int shift2 = shift1 >> (32-length);
-
-        return shift2;
+    private Bits() {
     }
 
-    public static int extractUnsigned(int value, int start, int length){
-        if (start + length >= 31){
-            throw new IllegalArgumentException();
-        }
-        int shift1 = value << (32-(start+length));
-        int shift2 = shift1 >>> (32-length);
+    public static int extractSigned(int value, int start, int length) {
+        int range = start + length;
 
-        return shift2;
+        if (range >= 0 & range <= 31) {
+            int shift1 = value << (32 - (start + length));
+            int shift2 = shift1 >> (32 - length);
+            return shift2;
+        } else {
+            throw new IllegalArgumentException("Select a valid range (between 0 and 31 (included)");
+        }
     }
+
+    public static int extractUnsigned(int value, int start, int length) {
+        int range = start + length;
+
+        if (range >= 0 & range < 31) {
+            int shift1 = value << (32 - (start + length));
+            int shift2 = shift1 >> (32 - length);
+            return shift2;
+        } else {
+            throw new IllegalArgumentException("Select a valid range (between 0 and 31)");
+        }
+    }
+
 }
--- src/ch/epfl/javelo/Functions.java
+++ src/ch/epfl/javelo/Functions.java
@@ -1,6 +1,39 @@
 package ch.epfl.javelo;
 
+import java.util.function.DoubleUnaryOperator;
+
 public final class Functions {
+
     private Functions() {}
+
+    public static DoubleUnaryOperator constant(double y){
+        return (x) -> y;
+    }
+
+    public static DoubleUnaryOperator sampled(float[] samples, double xMax){
+
+        if (samples.length < 2 || xMax <= 0){
+            throw new IllegalArgumentException();
+        }
+
+        return (x) -> {
+            double interval = xMax/samples.length;
+            double sampled = 0;
+            if (x<0){
+                return samples[0];
+            } else if (x>=xMax){
+                return samples[samples.length-1];
+            }
+            for (int i=0; i<xMax; interval++){
+                if (i<=x && i+interval>=x){
+                    double y1 = samples[i+1];
+                    double y0 = samples[i];
+                    double xCoordinate = (x - i)/interval;
+                    sampled = Math2.interpolate(y0,y1,xCoordinate);
+                }
+            }
+            return sampled;
+        };
+    }
 
 }
