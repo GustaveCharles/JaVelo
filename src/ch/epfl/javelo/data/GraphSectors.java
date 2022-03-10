01390b9bee48e1d616eb5b2cd710e651fd296f12
“bcoispeau” <baudoin.coispeau@epfl.ch>
2022-03-11 00:12:29 +0100

    GraphSectors finie

--- src/ch/epfl/javelo/data/GraphSectors.java
+++ src/ch/epfl/javelo/data/GraphSectors.java
@@ -12,9 +12,8 @@ public record GraphSectors(ByteBuffer buffer) {
     private static final int OFFSET_LENGTH = OFFSET_NODE + Integer.BYTES;
     private static final int SECTORS_INTS = OFFSET_LENGTH + Short.BYTES;
     private final static int NUMBER_OF_SECTORS_PER_AXIS = 128;
-    private final static int sectorHeight = (int) HEIGHT/NUMBER_OF_SECTORS_PER_AXIS;
-    private final static int sectorWidth = (int) WIDTH/NUMBER_OF_SECTORS_PER_AXIS;
-
+    private final static double sectorHeight = HEIGHT/NUMBER_OF_SECTORS_PER_AXIS;
+    private final static double sectorWidth = WIDTH/NUMBER_OF_SECTORS_PER_AXIS;
 
     public List<Sector> sectorsInArea(PointCh center, double distance){
 
@@ -23,10 +22,10 @@ public record GraphSectors(ByteBuffer buffer) {
         double rightTopX = clamp(MIN_E, center.e() + distance, MAX_E);
         double rightTopY = clamp(MIN_N, center.n() + distance, MAX_N);
 
-        int minX = (int) Math.floor((leftBottomX - MIN_E)/sectorWidth);
-        int maxX = (int) Math.ceil(((rightTopX - MIN_E)/sectorWidth) - 1);
-        int minY = (int) Math.floor((leftBottomY - MIN_N)/sectorHeight);
-        int maxY = (int) Math.ceil(((rightTopY - MIN_N)/sectorHeight) - 1);
+        int minX = (int) ((leftBottomX - MIN_E)/sectorWidth);
+        int maxX = (int) Math.ceil(((rightTopX - MIN_E)/sectorWidth));
+        int minY = (int) ((leftBottomY - MIN_N)/sectorHeight);
+        int maxY = (int) Math.ceil(((rightTopY - MIN_N)/sectorHeight));
 
         List<Sector> sectors = new ArrayList<>();
 
