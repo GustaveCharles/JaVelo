b19b3a758471779ef65a6f4b3b7320cbab0def21
“bcoispeau” <baudoin.coispeau@epfl.ch>
2022-04-01 15:06:56 +0200

    RouteComputer finie et testée

--- src/ch/epfl/javelo/routing/RouteComputer.java
+++ src/ch/epfl/javelo/routing/RouteComputer.java
@@ -1,53 +1,76 @@
-//package ch.epfl.javelo.routing;
-//
-//import ch.epfl.javelo.Preconditions;
-//import ch.epfl.javelo.data.Graph;
-//import ch.epfl.javelo.data.GraphNodes;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public final class RouteComputer {
-//    private final Graph graph;
-//    private final CostFunction costFunction;
-//
-//    RouteComputer(Graph graph, CostFunction costFunction){
-//        this.graph = graph;
-//        this.costFunction = costFunction;
-//    }
-//
-//    // ATTENTION A L IMMUTABILITE
-//
-//    public Route bestRouteBetween(int startNodeId, int endNodeId){
-//        Preconditions.checkArgument(startNodeId != endNodeId);
-//
-//        List<Double> nodeDistanceFromOrigin = new ArrayList<>();
-//        //edegTargetNodeId : qui retourne l'identité du nœud destination de l'arête d'identité donnée,
-//        //double edgeLength(int edgeId), qui retourne la longueur, en mètres, de l'arête d'identité donnée,
-//        //int nodeOutDegree(int nodeId), qui retourne le nombre d'arêtes sortant du nœud d'identité donnée,
-//        //int nodeOutEdgeId(int nodeId, int edgeIndex), qui retourne l'identité de la edgeIndex-ième arête sortant
-//        // du nœud d'identité nodeId,
-//        //int nodeOutDegree(int nodeId), qui retourne le nombre d'arêtes sortant du nœud d'identité donnée,
-//
-//        List<Integer> unvisitedNode = new ArrayList<>();
-//        for (int i=startNodeId; i<=endNodeId; i++){
-//            unvisitedNode.add(i);
-//        }
-//
-//        for (int i=0; i<unvisitedNode.size(); i++){
-//            int id = unvisitedNode.get(i);
-//            double minEdgeLength = 0;
-//            int numberOfOutgoingEdges = graph.nodeOutDegree(id);
-//            double[] lengthOfEdges = new double[numberOfOutgoingEdges];
-//            for (int j = 0; j<numberOfOutgoingEdges; j++){
-//                int outgoingEdgeId = graph.nodeOutEdgeId(id, j);
-//                lengthOfEdges[j] = graph.edgeLength(outgoingEdgeId);
-//            }
-//            //Arrays.sort(lengthOfEdges);
-//            //minEdgeLength = lengthOfEdges[0]; en faisant ça on casse les index
-//            //FAIRE TRES ATTENTION A LA MODIFICATION D UNE LISTE PDT SON PARCOURS
-//            unvisitedNode.remove(i);
-//        }
-//    }
-//}
+package ch.epfl.javelo.routing;
+
+import ch.epfl.javelo.Preconditions;
+import ch.epfl.javelo.data.Graph;
+
+import java.util.*;
+
+public final class RouteComputer {
+    private final Graph graph;
+    private final CostFunction costFunction;
+
+    public RouteComputer(Graph graph, CostFunction costFunction){
+        this.graph = graph;
+        this.costFunction = costFunction;
+    }
+
+    public Route bestRouteBetween(int startNodeId, int endNodeId){
+        Preconditions.checkArgument(startNodeId != endNodeId);
+        int nbOfNode = graph.nodeCount();
+        double[] distance = new double[nbOfNode];
+        int[] predecessor = new int[nbOfNode];
+        Set<Integer> exploringNode = new HashSet<>();
+        exploringNode.add(startNodeId);
+        Arrays.fill(distance, Float.POSITIVE_INFINITY);
+        distance[startNodeId] = 0;
+
+        while (exploringNode.size()!= 0){
+            int N = getIndexOfMinValue(exploringNode, distance);
+            exploringNode.remove(N);
+            if (N == endNodeId){break;}
+            int numberOfOutgoingEdges = graph.nodeOutDegree(N);
+            for (int edgeIndex=0; edgeIndex<numberOfOutgoingEdges; edgeIndex++){
+                int outgoingEdgeId = graph.nodeOutEdgeId(N, edgeIndex);
+                int N2 = graph.edgeTargetNodeId(outgoingEdgeId);
+                double d = distance[N] + graph.edgeLength(outgoingEdgeId)*costFunction.costFactor(N,outgoingEdgeId);
+                if (d<distance[N2]){
+                    distance[N2] = d;
+                    predecessor[N2] = N;
+                    exploringNode.add(N2);
+                }
+            }
+        }
+
+        List<Edge> edges = new ArrayList<>();
+        int currentNode = endNodeId;
+        int predecessorNode = 0;
+        while (currentNode != startNodeId){
+            predecessorNode = predecessor[currentNode];
+            int numberOfNodes = graph.nodeOutDegree(currentNode);
+            for (int j = 0; j < numberOfNodes; j++) {
+                int outgoingEdgeId = graph.nodeOutEdgeId(currentNode, j);
+                int node = graph.edgeTargetNodeId(outgoingEdgeId);
+                if (node == predecessorNode) {
+                    edges.add(Edge.of(graph,outgoingEdgeId, predecessorNode, currentNode));
+                }
+            }
+            currentNode = predecessorNode;
+        }
+
+        Collections.reverse(edges);
+
+        return new SingleRoute(edges);
+    }
+
+    private static int getIndexOfMinValue(Set<Integer> exploringNode, double[] distance) {
+        double minValue = Double.POSITIVE_INFINITY;
+        int N = 0;
+        for (Integer i : exploringNode) {
+            if (distance[i] < minValue) {
+                minValue = distance[i];
+                N = i;
+            }
+        }
+        return N;
+    }
+}
