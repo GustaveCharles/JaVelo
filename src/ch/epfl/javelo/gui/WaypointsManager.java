110b0b6d4e92c3ed78be46cea2e690abde147f7f
“bcoispeau” <baudoin.coispeau@epfl.ch>
2022-04-26 11:28:39 +0200

    Etape 7 finie et testée, embryon de partie 2 étape 8 et correction esthétique du code pour bits et webMercator

--- src/ch/epfl/javelo/gui/WaypointsManager.java
+++ src/ch/epfl/javelo/gui/WaypointsManager.java
@@ -1,10 +1,14 @@
 package ch.epfl.javelo.gui;
 
 import ch.epfl.javelo.data.Graph;
+import ch.epfl.javelo.projection.PointCh;
 import ch.epfl.javelo.projection.PointWebMercator;
+import javafx.beans.Observable;
 import javafx.beans.property.ObjectProperty;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.SVGPath;
 
@@ -24,34 +28,10 @@ public final class WaypointsManager {
         this.listOfWayPoint = listOfWayPoint;
         this.stringConsumer = stringConsumer;
         pane = new Pane();
-
-        for (int i = 0; i < listOfWayPoint.size(); i++) {
-            Group wayPointGroup = new Group();
-            wayPointGroup.getStyleClass().add("pin");
-            SVGPath path1 = new SVGPath();
-            path1.getStyleClass().add("pin_outside");
-            path1.setContent("M-8-20C-5-14-2-7 0 0 2-7 5-14 8-20 20-40-20-40-8-20");
-            SVGPath path2 = new SVGPath();
-            path2.getStyleClass().add("pin_inside");
-            path2.setContent("M0-23A1 1 0 000-29 1 1 0 000-23");
-
-            if (i == 0) {
-                path1.getStyleClass().add("first");
-            } else if (i == listOfWayPoint.size() - 1) {
-                path1.getStyleClass().add("last");
-            } else {
-                path1.getStyleClass().add("middle");
-            }
-
-            wayPointGroup.setLayoutX(property.getValue().viewX((PointWebMercator.ofPointCh(
-                    listOfWayPoint.get(i).crossingPosition()))));
-            wayPointGroup.setLayoutY(property.getValue().viewY((PointWebMercator.ofPointCh(
-                    listOfWayPoint.get(i).crossingPosition()))));
-
-            wayPointGroup.getChildren().addAll(path1, path2);
-            pane.getChildren().add(wayPointGroup);
-        }
-
+        pane.setPickOnBounds(false);
+        updatePane();
+        listOfWayPoint.addListener((Observable o) -> updatePane());
+//      property.addListener((Observable o) -> );
     }
 
     public Pane pane() {
@@ -59,7 +39,67 @@ public final class WaypointsManager {
     }
 
     public void addWaypoint(int x, int y) {
+        PointCh point = property.getValue().pointAt(x, y).toPointCh();
+        int closestPointId = graph.nodeClosestTo(point, 1000);
 
+        pane.setOnMouseDragged(e1 -> {
+            //deplacement
+        });
+        pane.setOnMouseReleased(e2 -> {
+            if (e2.isStillSincePress()) {
+                if (closestPointId != -1) {
+                    listOfWayPoint.add(new Waypoint(point, closestPointId));
+                } else {
+                    stringConsumer.accept("No road nearby!");
+                }
+            } else {
+                //delete
+                //
+            }
+        });
+    }
+
+    private void updatePane() {
+        for (int i = 0; i < listOfWayPoint.size(); i++) {
+            SVGPath path1 = new SVGPath();
+            path1.getStyleClass().add("pin_outside");
+            path1.setContent("M-8-20C-5-14-2-7 0 0 2-7 5-14 8-20 20-40-20-40-8-20");
+            SVGPath path2 = new SVGPath();
+            path2.getStyleClass().add("pin_inside");
+            path2.setContent("M0-23A1 1 0 000-29 1 1 0 000-23");
+
+            Group wayPointGroup = new Group(path1, path2);
+            wayPointGroup.getStyleClass().add("pin");
+
+            if (i == 0) {
+                wayPointGroup.getStyleClass().add("first");
+            } else if (i == listOfWayPoint.size() - 1) {
+                wayPointGroup.getStyleClass().add("last");
+            } else {
+                wayPointGroup.getStyleClass().add("middle");
+            }
+
+            relocate(wayPointGroup, i);
+            pane.getChildren().add(wayPointGroup);
+            setEvent(wayPointGroup, i);
+        }
+    }
+
+    private void relocate(Group wayPointGroup, int index) {
+        wayPointGroup.setLayoutX(property.getValue().viewX((PointWebMercator.ofPointCh(
+                listOfWayPoint.get(index).crossingPosition()))));
+        wayPointGroup.setLayoutY(property.getValue().viewY((PointWebMercator.ofPointCh(
+                listOfWayPoint.get(index).crossingPosition()))));
+    }
+
+    private void setEvent(Group wayPointGroup, int index) {
+        wayPointGroup.setOnMouseClicked(e -> {
+            listOfWayPoint.remove(index);
+        });
+
+        wayPointGroup.setOnMouseDragged(e1 -> {
+            relocate(wayPointGroup, index);
+        });
     }
 }
 
