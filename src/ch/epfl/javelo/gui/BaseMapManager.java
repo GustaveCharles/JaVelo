c4a8ce13bbd7dcbaf9455dc8ca8d303d4b121280
GustaveCharles <gustave.charles--saigne@epfl.ch>
2022-04-29 13:05:31 +0200

    tout marche sauf le scroll

--- src/ch/epfl/javelo/gui/BaseMapManager.java
+++ src/ch/epfl/javelo/gui/BaseMapManager.java
@@ -1,15 +1,18 @@
 package ch.epfl.javelo.gui;
 
+import ch.epfl.javelo.Math2;
+import ch.epfl.javelo.projection.PointWebMercator;
 import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.canvas.Canvas;
 
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * manages the display and interaction with the basemap
@@ -43,8 +46,81 @@ public final class BaseMapManager {
         });
 
         redrawOnNextPulse();
-        //canvas.setOnMousePressed(e->redrawOnNextPulse());
-        canvas.setOnMouseClicked(e -> waypointsManager.addWaypoint(e.getX(),e.getY()));
+        canvas.heightProperty().addListener(o -> redrawOnNextPulse());
+        canvas.widthProperty().addListener(o -> redrawOnNextPulse());
+
+//        AtomicInteger xvalue = null;
+//        AtomicInteger yvalue = null;
+//
+//        canvas.setOnMouseClicked(e->{
+//            xvalue.set((int)e.getX());
+//            yvalue.set((int) e.getY());
+//        }
+//        );
+//
+//        canvas.setOnMouseDragged(e-> {
+//            property.set(new MapViewParameters(property.get().zoomLevel(),
+//                    e.getX()-xvalue.get(),e.getY()-yvalue.get()));
+//            redrawOnNextPulse();
+//        }
+//        );
+
+        ObjectProperty<Point2D> property1 = new SimpleObjectProperty<>();
+        pane.setOnMousePressed(e -> {
+            property1.set(new Point2D(e.getX(), e.getY()));
+            System.out.println("true");
+        });
+
+
+          //  property1.addListener(e -> System.out.println("Simple Object Property 1: " + e));
+
+            pane.setOnMouseDragged(e -> {
+                        Point2D pd = new Point2D(property1.get().getX(), property1.get().getY());
+                        Point2D pd1 = pd.subtract(e.getX(), e.getY());
+
+                        property.set(new MapViewParameters(property.get().zoomLevel(),
+                                property.get().xTopLeft()  - pd1.getX(),
+                                property.get().yTopLeft()  - pd1.getY()));
+                        redrawOnNextPulse();
+
+                 //       property.addListener(event -> System.out.println("Simple Object Property new top left X: " + (property.get().xTopLeft() + e.getX() + pd1.getX())));
+               // property.addListener(event -> System.out.println("Simple Object Property new top left Y: " + (property.get().yTopLeft() + e.getY() + pd1.getY())));
+
+                    }
+            );
+
+
+        pane.setOnScroll(e -> {
+           int newZoom = (int)Math2.clamp(8,property.get().zoomLevel() + e.getDeltaY(),19);
+            System.out.println(e.getDeltaY());
+            System.out.println(newZoom);
+
+
+            PointWebMercator newCoordinates  = PointWebMercator.of(property.get().zoomLevel(),property.get().xTopLeft(),property.get().yTopLeft());
+
+            double newX = newCoordinates.xAtZoomLevel(newZoom);
+            double newY = newCoordinates.yAtZoomLevel(newZoom);
+
+            property.set(new MapViewParameters(newZoom,
+                    newX,
+                    newY));
+
+            PointWebMercator mousePoint = property.get().pointAt(e.getX(),e.getY());
+
+            property.set(new MapViewParameters(newZoom,
+                    newX+ mousePoint.xAtZoomLevel(newZoom)-e.getX(),
+                   newY + mousePoint.xAtZoomLevel(newZoom)-e.getY()));
+
+           redrawOnNextPulse();
+
+        });
+
+        pane.setOnMouseReleased(e -> {
+                    if (e.isStillSincePress()) {
+                        waypointsManager.addWaypoint(e.getX(), e.getY());
+                    }
+                }
+        );
     }
 
     public Pane pane() {
@@ -57,12 +133,6 @@ public final class BaseMapManager {
         canvas.heightProperty().bind(pane.heightProperty());
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
-
-        //canvas.heightProperty().addListener(o -> System.out.printf("New canvas height: %.2f\n", canvas.getHeight()));
-        //canvas.widthProperty().addListener(o -> System.out.printf("New canvas width: %.2f\n", canvas.getWidth()));
-
-        canvas.heightProperty().addListener(o -> redrawOnNextPulse());
-        canvas.widthProperty().addListener(o -> redrawOnNextPulse());
 
         double x = property.get().xTopLeft();
         double y = property.get().yTopLeft();
