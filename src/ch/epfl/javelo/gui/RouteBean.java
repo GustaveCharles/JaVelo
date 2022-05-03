88d81522f0c8a7132078f631db6b2e4db720fbef
“bcoispeau” <baudoin.coispeau@epfl.ch>
2022-05-03 10:40:01 +0200

    commit intermédiaire RouteBean

--- src/ch/epfl/javelo/gui/RouteBean.java
+++ src/ch/epfl/javelo/gui/RouteBean.java
@@ -1,58 +1,65 @@
 package ch.epfl.javelo.gui;
 
-import ch.epfl.javelo.Preconditions;
-import ch.epfl.javelo.projection.PointCh;
 import ch.epfl.javelo.routing.*;
 import javafx.beans.Observable;
 import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.util.Pair;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import javax.swing.*;
+import java.util.*;
 
 public final class RouteBean {
 
-    //Lorsque la liste des points de passage ne contient pas au moins 2 éléments,
-    // ou s'il existe au moins une paire de points de passage entre lesquels aucun itinéraire
-    // ne peut être trouvé, alors ni l'itinéraire ni son profil n'existent, et les propriétés
-    // correspondantes contiennent null.
-
     private final RouteComputer routeComputer;
-    private final DoubleProperty highlightedPosition = new SimpleDoubleProperty();
-    private ObjectProperty<Route> route = new SimpleObjectProperty<>();
-    private ObservableList<Waypoint> waypoints = FXCollections.observableArrayList();
-    private ObjectProperty<ElevationProfile> elevationProfile = new SimpleObjectProperty();
+    private final DoubleProperty highlightedPosition;
+    private final ObjectProperty<Route> route;
+    private final ObservableList<Waypoint> waypoints;
+    private final ObjectProperty<ElevationProfile> elevationProfile;
     private final static int MAX_LENGTH = 5;
+    private HashMap<Pair<Waypoint, Waypoint>, Route> map;
 
     public RouteBean(RouteComputer routeComputer) {
         this.routeComputer = routeComputer;
-        waypoints.addListener((Observable o) -> createSingleRoute());
+        map = new LinkedHashMap<>();
+        highlightedPosition = new SimpleDoubleProperty();
+        route = new SimpleObjectProperty<>();
+        waypoints = FXCollections.observableArrayList();
+        elevationProfile = new SimpleObjectProperty<>();
+        waypoints.addListener((Observable o) -> createRoute());
     }
 
-    ElevationProfile getElevationProfile() {
-        return ElevationProfileComputer.elevationProfile(route.getValue(), MAX_LENGTH);
-    }
-
-    public ReadOnlyObjectProperty<Route> getRoute() {
+    public ReadOnlyObjectProperty<Route> routeProperty() {
         return route;
     }
 
-    //highlighted
-    //routeProperty
+    public ReadOnlyObjectProperty<ElevationProfile> elevationProfileProperty() {
+        return elevationProfile;
+    }
 
-    private Route createSingleRoute() {
-        record nodePair(int nodeId1, int nodeId2) {
+    private void createRoute() {
+        //map
+        if (waypoints.size() >= 2) {
+            List<Route> listRoute = new ArrayList<>();
+            for (int i = 1; i < waypoints.size(); i++) {
+                int point1 = waypoints.get(i - 1).closestJaVeloNode();
+                int point2 = waypoints.get(i).closestJaVeloNode();
+                Route r = routeComputer.bestRouteBetween(point1, point2);
+                if (r != null) {
+                    listRoute.add(r);
+                } else {
+                    route.setValue(null);
+                    elevationProfile.setValue(null);
+                }
+            }
+            MultiRoute m = new MultiRoute(listRoute);
+            route.setValue(m);
+            elevationProfile.setValue(ElevationProfileComputer.elevationProfile(m, MAX_LENGTH));
+        } else {
+            route.setValue(null);
+            elevationProfile.setValue(null);
         }
-        List<Route> listRoute = new ArrayList<>();
-        for (int i = 1; i < waypoints.size(); i++) {
-            int point1 = waypoints.get(i - 1).closestJaVeloNode();
-            int point2 = waypoints.get(i).closestJaVeloNode();
-            Route r = routeComputer.bestRouteBetween(point1, point2);
-            listRoute.add(r);
-        }
-        return new MultiRoute(listRoute);
     }
 
     public double getHighlightedPosition() {
@@ -71,15 +78,17 @@ public final class RouteBean {
         return highlightedPosition.getValue();
     }
 
-    public void addWayPoints(Waypoint waypoint) {
-        waypoints.add(waypoint);
+
+    public void addWaypoints(Waypoint waypoint) {
+//        if (!map.containsKey(waypoint) && !waypoints.contains(waypoint)){
+//            map.put()
+//            waypoints.add(waypoint);
+//        }
     }
 
-    //Lorsque la liste des points de passage ne contient pas au moins 2 éléments,
-    // ou s'il existe au moins une paire de points de passage entre lesquels aucun itinéraire
-    // ne peut être trouvé, alors ni l'itinéraire ni son profil n'existent, et les propriétés
-    // correspondantes contiennent null.
-
+    public ObservableList<Waypoint> waypointsProperty() {
+        return waypoints;
+    }
 }
 
 
