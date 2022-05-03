f1280c88c06c5dcfbc6aefb2e61b9d72ad7b23c4
“bcoispeau” <baudoin.coispeau@epfl.ch>
2022-05-03 09:03:12 +0200

    RouteBean pas finie

--- src/ch/epfl/javelo/gui/RouteBean.java
+++ src/ch/epfl/javelo/gui/RouteBean.java
@@ -1,15 +1,13 @@
 package ch.epfl.javelo.gui;
 
 import ch.epfl.javelo.Preconditions;
-import ch.epfl.javelo.routing.ElevationProfile;
-import ch.epfl.javelo.routing.Route;
-import ch.epfl.javelo.routing.RouteComputer;
+import ch.epfl.javelo.routing.*;
 import javafx.beans.Observable;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.ReadOnlyObjectProperty;
-import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
+import java.util.List;
 
 public final class RouteBean {
 
@@ -18,47 +16,65 @@ public final class RouteBean {
     // ne peut être trouvé, alors ni l'itinéraire ni son profil n'existent, et les propriétés
     // correspondantes contiennent null.
 
-
     private final RouteComputer routeComputer;
-    DoubleProperty highlightedPosition = new SimpleDoubleProperty();
-    ObjectProperty<Route> r = (ObjectProperty<Route>) route();
-    ObjectProperty<ElevationProfile> elevation = (ObjectProperty<ElevationProfile>) elevationProfile();
+    private final DoubleProperty highlightedPosition = new SimpleDoubleProperty();
+    private ObjectProperty<Route> route = new SimpleObjectProperty<>();
+    private ObservableList<Waypoint> waypoints = FXCollections.observableArrayList();
+    private ObjectProperty<ElevationProfile> elevationProfile = new SimpleObjectProperty();
 
     public RouteBean(RouteComputer routeComputer) {
         this.routeComputer = routeComputer;
+        route.addListener((Observable o) ->);
 
     }
 
-    private ObservableList<Waypoint> waypoints() {
-        Preconditions.checkArgument(waypoints().size() >= 2);
-        waypoints().addListener((Observable o) -> elevationProfile(), route());
+    ElevationProfile getElevationProfile() {
+        return ElevationProfileComputer.elevationProfile(route(), 5);
     }
 
-    private ReadOnlyObjectProperty<ElevationProfile> elevationProfile() {
+    public ReadOnlyObjectProperty<Route> getRoute() {
+        return route;
     }
 
-    private ReadOnlyObjectProperty<Route> route() {
+    public void setRoute(Route route) {
+        this.route.set(route);
+    }
+
+    private Route route() {
+        return route.getValue();
+    }
+
+    //highlighted
+    //routeProperty
+
+    private Route createSingleRoute() {
+
+    }
+
+    public double getHighlightedPosition() {
+        return highlightedPosition.get();
+    }
+
+    public void setHighlightedPosition(double newHighlightedPosition) {
+        highlightedPosition.setValue(newHighlightedPosition);
     }
 
     public DoubleProperty highlightedPositionProperty() {
-        return highlightedPositionProperty();
+        return highlightedPosition;
     }
 
     public double highlightedPosition() {
-        return highlightedPositionProperty().getValue();
+        return highlightedPosition.getValue();
     }
 
-    public void setHighlightedPosition(DoubleProperty newHighlightedPosition) {
-        highlightedPositionProperty().setValue(newHighlightedPosition.getValue());
+    public void addWayPoints(Waypoint waypoint) {
+        waypoints.add(waypoint);
     }
 
-    private ReadOnlyObjectProperty<Route> getRouteProperty() {
-        return r;
-    }
-
-    private ReadOnlyObjectProperty<ElevationProfile> getElevationProfileProperty() {
-        return elevation;
-    }
+    //Lorsque la liste des points de passage ne contient pas au moins 2 éléments,
+    // ou s'il existe au moins une paire de points de passage entre lesquels aucun itinéraire
+    // ne peut être trouvé, alors ni l'itinéraire ni son profil n'existent, et les propriétés
+    // correspondantes contiennent null.
 
 }
 
