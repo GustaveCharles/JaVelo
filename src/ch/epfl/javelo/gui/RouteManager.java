88fbcd8803c94a562925e91ca7e3cf601a6a970f
GustaveCharles <gustave.charles--saigne@epfl.ch>
2022-05-06 11:20:00 +0200

    routeManager presque finie

--- src/ch/epfl/javelo/gui/RouteManager.java
+++ src/ch/epfl/javelo/gui/RouteManager.java
@@ -5,6 +5,7 @@ import ch.epfl.javelo.projection.PointWebMercator;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polyline;
@@ -33,63 +34,68 @@ public final class RouteManager {
         this.property = property;
         this.errors = errors;
 
-         this.routeNodes = new ArrayList<>();
-         this.routeNodesDouble = new ArrayList<>();
+        this.routeNodes = new ArrayList<>();
+        this.routeNodesDouble = new ArrayList<>();
 
         pane = new Pane(line, circle);
         // pane.getChildren().add(line);
         //pane.getChildren().add(circle);
         pane.setPickOnBounds(false);
 
+
         line.setId("route");
         circle.setId("highlight");
 
         //si la propriété route de routeBean contient null
-        if (routeBean.routeProperty() == null) {
-            line.setVisible(false);
-            circle.setVisible(false);
-        }
 
-        line.setLayoutX(line.getLayoutX() - property.get().xTopLeft());
-        line.setLayoutY(line.getLayoutY() - property.get().yTopLeft());
+        line.setVisible(false);
+        circle.setVisible(false);
 
-        if(routeBean.routeProperty().get() != null){
-          //  pointsSequence();
-        }
-        routeBean.routeProperty().addListener(e -> {
-            //rendre visible invisible disque et ligne
-        });
-        property.addListener(e -> {
-            //rendre visible invisible disque et ligne
-        });
 
-        property.addListener((e,oV,nV) -> {
-            if(oV.zoomLevel() != nV.zoomLevel()){
-             //   pointsSequence();
+        routeBean.routeProperty().addListener((e, oV, nV) -> {
+
+            if ((e == null)) {
+                line.setVisible(false);
+                circle.setVisible(false);
             }
 
-            line.setLayoutX(-oV.topLeft().getX());
-            line.setLayoutX(-oV.topLeft().getY());
+
+            if (e != null || oV != nV) {
+                line.getPoints().clear();
+                pointsSequence();
+                line.setLayoutX(-property.get().xTopLeft());
+                line.setLayoutY(-property.get().yTopLeft());
+
+                routeBean.setHighlightedPosition(1000);
+
+                PointWebMercator point = PointWebMercator.
+                        ofPointCh(routeBean.routeProperty().get().pointAt(routeBean.getHighlightedPosition()));
+
+                circle.setCenterX(point.xAtZoomLevel(property.get().zoomLevel()) - property.get().xTopLeft());
+                circle.setCenterY(point.yAtZoomLevel(property.get().zoomLevel()) - property.get().yTopLeft());
+
+
+                line.setVisible(true);
+                circle.setVisible(true);
+            }
         });
 
-//        circle.visibleProperty().addListener((e, oV, nV) -> {
-//            if (nV != oV) {
-//
-//            } else {
-//                circle.setCenterX(routeBean.highlightedPositionProperty().get());
-//                circle.setCenterY(routeBean.highlightedPosition.get().getY);
-//            }
-//            if (e.getValue() == true) {
-//                circle.setVisible(true);
-//            }
-//        });
 
-        line.visibleProperty().addListener((e, oV, nV) -> {
+        property.addListener((e, oV, nV) -> {
+            if (oV.zoomLevel() != nV.zoomLevel()) {
+                pointsSequence();
+                line.setLayoutX(-nV.xTopLeft());
+                line.setLayoutY(-nV.yTopLeft());
+            }
+            if (oV.zoomLevel() == nV.zoomLevel()) {
+                line.setLayoutX(line.getLayoutX() + oV.xTopLeft() - nV.xTopLeft());
+                line.setLayoutY(line.getLayoutY() + oV.yTopLeft() - nV.yTopLeft());
+                circle.setCenterX(circle.getCenterX() + oV.xTopLeft() - nV.xTopLeft());
+                circle.setCenterY(circle.getCenterY() + oV.yTopLeft() - nV.yTopLeft());
+            }
 
-            // if(property.get().zoomLevel() == ){
-
-            // }
         });
+
         handler();
 
     }
@@ -117,17 +123,17 @@ public final class RouteManager {
 
 
     private void pointsSequence() {
-        System.out.println("coucou");
 
         routeNodes = new ArrayList<>(routeBean.routeProperty().get().points());
-
         routeNodes.forEach(o -> {
-                    routeNodesDouble.add(o.lat());
-                    routeNodesDouble.add(o.lon());
-                }
-        );
+            PointWebMercator pointWebMercator = PointWebMercator.ofPointCh(o);
+            routeNodesDouble.add(pointWebMercator.xAtZoomLevel(property.get().zoomLevel()));
+            routeNodesDouble.add(pointWebMercator.yAtZoomLevel(property.get().zoomLevel()));
+
+        });
 
         line.getPoints().addAll(routeNodesDouble);
+
     }
 
 }
