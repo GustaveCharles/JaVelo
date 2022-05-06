21296370c01f8629fe6b1f69b0a31ae7f2942078
GustaveCharles <gustave.charles--saigne@epfl.ch>
2022-05-06 14:11:01 +0200

    route manager finie je crois

--- src/ch/epfl/javelo/gui/RouteManager.java
+++ src/ch/epfl/javelo/gui/RouteManager.java
@@ -5,7 +5,6 @@ import ch.epfl.javelo.projection.PointWebMercator;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.geometry.Point2D;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polyline;
@@ -17,19 +16,19 @@ import java.util.function.Consumer;
 public final class RouteManager {
 
     private final Pane pane;
-    private Polyline line;
+    private final Polyline line;
     private final Circle circle;
-    private RouteBean routeBean;
-    private final ObjectProperty<MapViewParameters> property;
+    private final RouteBean routeBean;
+    private final ObjectProperty<MapViewParameters> mapParameters;
     private final Consumer<String> errors;
     private List<PointCh> routeNodes;
-    private List<Double> routeNodesDouble;
+    private final List<Double> routeNodesDouble;
 
-    public RouteManager(RouteBean routeBean, SimpleObjectProperty<MapViewParameters> property, Consumer<String> errors) {
+    public RouteManager(RouteBean routeBean, SimpleObjectProperty<MapViewParameters> mapParameters, Consumer<String> errors) {
         this.line = new Polyline();
         this.routeBean = routeBean;
         this.circle = new Circle(5);
-        this.property = property;
+        this.mapParameters = mapParameters;
         this.errors = errors;
 
         this.routeNodes = new ArrayList<>();
@@ -47,46 +46,74 @@ public final class RouteManager {
         line.setVisible(false);
         circle.setVisible(false);
 
+        routeBean.highlightedPositionProperty().addListener( e -> {
+            PointWebMercator point = PointWebMercator.
+                    ofPointCh(routeBean.routeProperty().get().pointAt(routeBean.getHighlightedPosition()));
 
-        routeBean.routeProperty().addListener((e, oV, nV) -> {
+            circle.setCenterX(point.xAtZoomLevel(mapParameters.get().zoomLevel()) - mapParameters.get().xTopLeft());
+            circle.setCenterY(point.yAtZoomLevel(mapParameters.get().zoomLevel()) - mapParameters.get().yTopLeft());
+        });
 
-            if ((nV == null)) {
-                System.out.println("coucou");
+        //rendre invisible la ligne lorsque lorsque itinéraire est nul
+        //rendre visible la ligne lorsque l'itinéraire est pas nul
+        // rendre invisible le disque lorsque itinéraire est nul
+        //rendre visible le disque lorsque itinéraire est non nul
+        routeBean.routeProperty().addListener(e -> {
+
+            if ((routeBean.routeProperty().get() == null)) {
                 line.setVisible(false);
                 circle.setVisible(false);
             }
 
-
-            if (e != null || oV != nV) {
-
-                pointsSequence();
-                line.setLayoutX(-property.get().xTopLeft());
-                line.setLayoutY(-property.get().yTopLeft());
-
-                routeBean.setHighlightedPosition(1000);
-
-                PointWebMercator point = PointWebMercator.
-                        ofPointCh(routeBean.routeProperty().get().pointAt(routeBean.getHighlightedPosition()));
-
-                circle.setCenterX(point.xAtZoomLevel(property.get().zoomLevel()) - property.get().xTopLeft());
-                circle.setCenterY(point.yAtZoomLevel(property.get().zoomLevel()) - property.get().yTopLeft());
-
+            if (routeBean.routeProperty().get() != null) {
 
                 line.setVisible(true);
                 circle.setVisible(true);
             }
         });
 
-        property.addListener((e, oV, nV) -> {
+        //reconstruire ligne lorsque itinéraire change
+        // reconstruire le disque lorsque les paramètres de la carte changent (xtopleft ytopleft)
+        routeBean.routeProperty().addListener((e,oV,nV) -> {
+            if(oV != nV && e!=null){
+                pointsSequence();
+                line.setLayoutX(-mapParameters.get().xTopLeft());
+                line.setLayoutY(-mapParameters.get().yTopLeft());
+
+                routeBean.setHighlightedPosition(1000);
+
+                PointWebMercator point = PointWebMercator.
+                        ofPointCh(routeBean.routeProperty().get().pointAt(routeBean.getHighlightedPosition()));
+
+                circle.setCenterX(point.xAtZoomLevel(mapParameters.get().zoomLevel()) - mapParameters.get().xTopLeft());
+                circle.setCenterY(point.yAtZoomLevel(mapParameters.get().zoomLevel()) - mapParameters.get().yTopLeft());
+            }
+        });
+
+        //repositionner ligne lorsque la carte glissée
+        //reconstruire ligne lors de nouveau zoom
+        // reconstruire le disque lorsque les paramètres de la carte changent (zoom)
+
+        mapParameters.addListener((e, oV, nV) -> {
+
             if (oV.zoomLevel() != nV.zoomLevel() && e!=null) {
 
                 pointsSequence();
                 line.setLayoutX(-nV.xTopLeft());
                 line.setLayoutY(-nV.yTopLeft());
+
+                PointWebMercator point = PointWebMercator.
+                        ofPointCh(routeBean.routeProperty().get().pointAt(routeBean.getHighlightedPosition()));
+
+                circle.setCenterX(point.xAtZoomLevel(nV.zoomLevel()) - nV.xTopLeft());
+                circle.setCenterY(point.yAtZoomLevel(nV.zoomLevel()) - nV.yTopLeft());
+
             }
             if (oV.zoomLevel() == nV.zoomLevel()) {
+
                 line.setLayoutX(line.getLayoutX() + oV.xTopLeft() - nV.xTopLeft());
                 line.setLayoutY(line.getLayoutY() + oV.yTopLeft() - nV.yTopLeft());
+
                 circle.setCenterX(circle.getCenterX() + oV.xTopLeft() - nV.xTopLeft());
                 circle.setCenterY(circle.getCenterY() + oV.yTopLeft() - nV.yTopLeft());
             }
@@ -106,7 +133,7 @@ public final class RouteManager {
 
         circle.setOnMouseClicked(e -> {
                     Point2D point2D = circle.localToParent(e.getX(), e.getY());
-                    PointCh point = property.getValue().pointAt(point2D.getX(), point2D.getY()).toPointCh();
+                    PointCh point = mapParameters.getValue().pointAt(point2D.getX(), point2D.getY()).toPointCh();
                     int closestPointId = routeBean.routeProperty().get()
                             .nodeClosestTo(routeBean.highlightedPosition());
                     if (closestPointId != -1) {
@@ -131,8 +158,8 @@ public final class RouteManager {
         routeNodes = new ArrayList<>(routeBean.routeProperty().get().points());
         routeNodes.forEach(o -> {
             PointWebMercator pointWebMercator = PointWebMercator.ofPointCh(o);
-            routeNodesDouble.add(pointWebMercator.xAtZoomLevel(property.get().zoomLevel()));
-            routeNodesDouble.add(pointWebMercator.yAtZoomLevel(property.get().zoomLevel()));
+            routeNodesDouble.add(pointWebMercator.xAtZoomLevel(mapParameters.get().zoomLevel()));
+            routeNodesDouble.add(pointWebMercator.yAtZoomLevel(mapParameters.get().zoomLevel()));
 
         });
 
