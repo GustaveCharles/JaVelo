26a92a7f7609b0251f627927c8997a1465f75c72
“bcoispeau” <baudoin.coispeau@epfl.ch>
2022-05-15 14:05:36 +0200

    RouteBean correction pour étape 11

--- src/ch/epfl/javelo/gui/RouteBean.java
+++ src/ch/epfl/javelo/gui/RouteBean.java
@@ -11,6 +11,8 @@ import java.util.*;
 
 import static java.lang.Double.NaN;
 
+//TODO vérifier si la correction à la ligne 68 est correcte
+
 /**
  * Represents a JavaFX bean for a route
  *
@@ -63,7 +65,10 @@ public final class RouteBean {
             for (int i = 1; i < waypoints.size(); i++) {
                 int point1 = waypoints.get(i - 1).closestJaVeloNode();
                 int point2 = waypoints.get(i).closestJaVeloNode();
-                if (!map.containsKey(new Pair<>(point1, point2)) & point1 != point2) {
+                if (point1 == point2) {
+                    return;
+                }
+                if (!map.containsKey(new Pair<>(point1, point2))) {
                     Route r = routeComputer.bestRouteBetween(point1, point2);
                     if (r == null) {
                         setRouteAndElevation(null, null);
