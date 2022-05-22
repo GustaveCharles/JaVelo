fdec604e07c89185d5ec356471b4294c10735d85
GustaveCharles <gustave.charles--saigne@epfl.ch>
2022-05-22 11:03:02 +0200

    bugs

--- src/ch/epfl/javelo/gui/AnnotatedMapManager.java
+++ src/ch/epfl/javelo/gui/AnnotatedMapManager.java
@@ -14,54 +14,49 @@ import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
-
-import java.nio.file.Path;
 import java.util.function.Consumer;
 
 public final class AnnotatedMapManager {
 
-    private final Graph graph;
-    private final TileManager tileManager;
     private final RouteBean routeBean;
-    private final Consumer<String> stringConsumer;
     private final StackPane mainPane;
-    private BaseMapManager baseMapManager;
-    private RouteManager routeManager;
-    private WaypointsManager waypointsManager;
-    private SimpleObjectProperty<Point2D> point2DProperty;
-    private DoubleProperty mouseProperty;
+    private final SimpleObjectProperty<Point2D> point2DProperty;
+    private final DoubleProperty mouseProperty;
+    private final SimpleObjectProperty<MapViewParameters> mapViewParametersP;
 
+    public final static int MAX_DISTANCE_PIXELS = 15;
     public final static MapViewParameters MAP_VIEW_PARAMETERS =
             new MapViewParameters(12, 543200, 370650);
 
-    //MapViewParameters c'est bien??
     public AnnotatedMapManager(Graph graph, TileManager tileManager, RouteBean routeBean, Consumer<String> stringConsumer) {
 
-        this.graph = graph;
-        this.tileManager = tileManager;
         this.routeBean = routeBean;
-        this.stringConsumer = stringConsumer;
-        double mouse = Double.NaN;
-        this.mouseProperty = new SimpleDoubleProperty(mouse);
-        Point2D point2D = Point2D.ZERO;
-        this.point2DProperty = new SimpleObjectProperty<>(point2D);
-        SimpleObjectProperty<MapViewParameters> mapViewParametersP =
-                new SimpleObjectProperty<>(MAP_VIEW_PARAMETERS);
+        this.mouseProperty = new SimpleDoubleProperty(Double.NaN);
+        this.point2DProperty = new SimpleObjectProperty<>(Point2D.ZERO);
+        this.mapViewParametersP = new SimpleObjectProperty<>(MAP_VIEW_PARAMETERS);
 
-        this.routeManager = new RouteManager(routeBean, mapViewParametersP);
-        this.waypointsManager = new WaypointsManager(graph, mapViewParametersP, routeBean.waypointsProperty(), stringConsumer);
-        this.baseMapManager = new BaseMapManager(tileManager, waypointsManager, mapViewParametersP);
+        RouteManager routeManager = new RouteManager(routeBean, mapViewParametersP);
+        WaypointsManager waypointsManager = new WaypointsManager(graph, mapViewParametersP, routeBean.waypointsProperty(), stringConsumer);
+        BaseMapManager baseMapManager = new BaseMapManager(tileManager, waypointsManager, mapViewParametersP);
 
-
-        this.mainPane =
-                new StackPane(baseMapManager.pane(),
-                        routeManager.pane(), waypointsManager.pane());
+        this.mainPane = new StackPane(baseMapManager.pane(), routeManager.pane(), waypointsManager.pane());
         mainPane.getStylesheets().add("map.css");
 
+        handler();
+    }
 
+    public Pane pane() {
+        return mainPane;
+    }
+
+    public ReadOnlyDoubleProperty mousePositionOnRouteProperty() {
+        return mouseProperty;
+    }
+
+    private void handler() {
         mouseProperty.bind(Bindings.createDoubleBinding(
                 () -> {
-                    if(routeBean.routeProperty().get() != null && point2DProperty.get() != null){
+                    if (routeBean.routeProperty().get() != null && point2DProperty.get() != null) {
                         Point2D point2DMouse = point2DProperty.get();
                         PointWebMercator pointWebMercator = mapViewParametersP.get().pointAt(point2DMouse.getX(), point2DMouse.getY());
                         PointCh pointCh = pointWebMercator.toPointCh();
@@ -75,27 +70,18 @@ public final class AnnotatedMapManager {
                         double y = mapViewParametersP.get().viewY(PointWebMercator.ofPointCh(point.point()));
                         Point2D point2D1 = new Point2D(x, y);
 
-                        if (Math2.norm(point2DMouse.getX() - point2D1.getX(), point2DMouse.getY() - point2D1.getY()) <= 15) {
+                        if (Math2.norm(point2DMouse.getX() - point2D1.getX(),
+                                point2DMouse.getY() - point2D1.getY()) <= MAX_DISTANCE_PIXELS) {
                             return point.position();
                         } else return Double.NaN;
-                    }else return Double.NaN;
+                    } else return Double.NaN;
                 },
-                 point2DProperty,routeBean.routeProperty(),mapViewParametersP
+                point2DProperty, routeBean.routeProperty(), mapViewParametersP
         ));
 
 
         mainPane.setOnMouseMoved(e -> point2DProperty.set(new Point2D(e.getX(), e.getY())));
 
         mainPane.setOnMouseExited(e -> point2DProperty.set(null));
-
-
-    }
-
-    public Pane pane() {
-        return mainPane;
-    }
-
-    public ReadOnlyDoubleProperty mousePositionOnRouteProperty() {
-        return mouseProperty;
     }
 }
